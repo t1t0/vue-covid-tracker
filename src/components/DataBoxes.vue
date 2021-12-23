598c8715f4ef24657085caf1804fdd4d6c0ddbd7
Viktor Rodriguez <viktor.a.rodriguez.o@gmail.com>
2021-12-23 12:52:04 -0400

    Update DataBoxes.vue

--- src/components/DataBoxes.vue
+++ src/components/DataBoxes.vue
@@ -1,7 +1,31 @@
 <template>
     <div class="grid md:grid-cols-2 gap-4">
+        <!-- Box 1 -->
         <div class="shadow-md bg-blue-100 p-10 text-center rounded">
             <h3 class="text-3xl text-blue-900 font-bold mb-4">Cases</h3>
+            <div class="text-2xl mb-4">
+                <span class="font-bold">New:</span>
+                {{ numberWithCommas(stats.NewConfirmed) }}
+            </div>
+            <div class="text-2xl mb-4">
+                <span class="font-bold">Total:</span>
+                {{ numberWithCommas(stats.TotalConfirmed) }}
+            </div>
+            
+        </div>
+
+        <!-- Box 2 -->
+        <div class="shadow-md bg-red-100 p-10 text-center rounded">
+            <h3 class="text-3xl text-blue-900 font-bold mb-4">Death</h3>
+            <div class="text-2xl mb-4">
+                <span class="font-bold">New:</span>
+                {{ numberWithCommas(stats.NewDeaths) }}
+            </div>
+            <div class="text-2xl mb-4">
+                <span class="font-bold">Total:</span>
+                {{ numberWithCommas(stats.TotalDeaths) }}
+            </div>
+            
         </div>
     </div>
 </template>
@@ -9,6 +33,11 @@
 <script>
 export default {
     name: 'DataBoxes',
-    props: ['stats']
+    props: ['stats'],
+    methods: {
+        numberWithCommas(x){
+            return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+        }
+    }
 }
 </script>
