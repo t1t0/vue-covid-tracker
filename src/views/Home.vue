f87765a4b20d354a9eb5ff930e35f6234eec1c8d
Viktor Rodriguez <viktor.a.rodriguez.o@gmail.com>
2021-12-23 10:43:29 -0400

    Update Home.vue
    clean default components, scripts and styles

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,27 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    Hello World!
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    
+  },
+  methods: {
+    async fetchCovidData(){
+      const res = await fetch('https://api.covid19api.com/summary')
+      const data = await res.json()
+      return data
+    }
+  },
+  created(){
+    console.log('Started')
   }
 }
 </script>
