8194bec5a66522b81263114761aac1b69e0ec7af
Viktor Rodriguez <viktor.a.rodriguez.o@gmail.com>
2021-12-23 10:42:36 -0400

    Tailwind Installed

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    container: {
+      center: true,
+      padding: '2rem'
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
