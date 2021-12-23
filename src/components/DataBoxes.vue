cc0cdf1c278ed4e1a36079be5ff0a1488ed10293
Viktor Rodriguez <viktor.a.rodriguez.o@gmail.com>
2021-12-23 11:54:32 -0400

    Create DataBoxes.vue

---
+++ src/components/DataBoxes.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="grid md:grid-cols-2 gap-4">
+        <div class="shadow-md bg-blue-100 p-10 text-center rounded">
+            <h3 class="text-3xl text-blue-900 font-bold mb-4">Cases</h3>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'DataBoxes',
+    props: ['stats']
+}
+</script>
