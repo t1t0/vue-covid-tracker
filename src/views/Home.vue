7821b274cc8344f5682ded35ecc7c1d3e8c2e5b2
Viktor Rodriguez <viktor.a.rodriguez.o@gmail.com>
2021-12-23 11:54:29 -0400

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,36 @@
 <template>
-  <div class="home">
-    Hello World!
-  </div>
+  <main v-if="!loading">
+    <DataTitle :text="title" :dataDate="dataDate"/>
+    <DataBoxes :stats="stats"/>
+  </main>
+
+  <main class="flex flex-col align-center justify-center text-center" v-else>
+    <div class="text-gray-500 text-3xl mt-10 mb-6">
+      Fetching Data
+    </div>
+    <img :src="loadingImage" class="w-24 m-auto" alt="Loading icon">
+  </main>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import DataTitle from '@/components/DataTitle'
+import DataBoxes from '@/components/DataBoxes'
 
 export default {
   name: 'Home',
   components: {
-    
+    DataTitle, DataBoxes
+  },
+  data() {
+    return {
+      loading: true,
+      title: 'Global',
+      dataDate: '',
+      stats: {},
+      countries: [],
+      loadingImage: require('../assets/hourglass.gif')
+    }
   },
   methods: {
     async fetchCovidData(){
@@ -20,8 +39,13 @@ export default {
       return data
     }
   },
-  created(){
-    console.log('Started')
+  async created(){
+    const data = await this.fetchCovidData()
+
+    this.dataDate = data.Date
+    this.stats = data.Global
+    this.countries = data.countries
+    this.loading = false
   }
 }
 </script>
